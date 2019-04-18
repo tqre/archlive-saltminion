698dd3189e6b3769f24e909ee46fad51004c3fa4
tqre <tkuure@gmail.com>
2019-04-18 21:53:39 +0300

    Added master ip file and configured init to read master_ip and set id to its own ip address

--- minion_init.sh
+++ minion_init.sh
@@ -3,9 +3,12 @@
 echo Minion initialization script starting...
 
 cat > /etc/salt/minion << EOF
-master: <ip address>
-id: <minion name>
+master: $(cat master_ip)
+id: $(hostname -i)
 EOF
+
+echo Master IP is being set to $(cat master_ip)
+echo Minion ID is being to $(hostname -i)
 
 echo Starting the salt-minion service...
 systemctl start salt-minion
