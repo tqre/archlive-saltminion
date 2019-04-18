395b97a32c05b7bba10ade03313fb52a5b1a9f0b
tqre <tkuure@gmail.com>
2019-04-18 21:14:26 +0300

    Minion init script skeleton

--- minion_init.sh
+++ minion_init.sh
@@ -1,1 +1,11 @@
-echo Minion initialization script
+#!/bin/bash
+# (C) 2019 Tuomo Kuure, GPL 3
+echo Minion initialization script starting...
+
+cat > /etc/salt/minion << EOF
+master: <ip address>
+id: <minion name>
+EOF
+
+echo Starting the salt-minion service...
+systemctl start salt-minion
