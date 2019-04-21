5ddd71beb46ae908958b39c72ad1fe722eb429c6
tqre <tkuure@gmail.com>
2019-04-21 14:29:51 +0300

    added minion(ipaddress) to be the hostname

--- minion_init.sh
+++ minion_init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo Minion initialization script starting...
 
+echo "minion$(hostname -i)" > /etc/hostname
+
 cat > /etc/salt/minion << EOF
 master: $(cat /tmp/minion/master_ip)
 id: $(hostname -i)
@@ -14,6 +16,7 @@ EOF
 
 echo Master IP is set to $(cat /tmp/minion/master_ip)
 echo Minion ID is $(hostname -i)
+echo Minion hostname is $(hostname)
 
 echo Starting the salt-minion service...
 systemctl start salt-minion
