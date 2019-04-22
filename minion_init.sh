7cbbe990b46928d79109de69e0d297270e70fa1e
tqre <tkuure@gmail.com>
2019-04-22 11:35:57 +0300

    Minion name set

--- minion_init.sh
+++ minion_init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo Minion initialization script starting...
 
+hostname minion@$(hostname -i)
+
 cat > /etc/salt/minion << EOF
 master: $(cat /tmp/minion/master_ip)
-id: archliveminion$(hostname -i)
+id: $(hostname)
 startup_states: sls
 
 sls_list:
@@ -12,7 +14,7 @@ sls_list:
 EOF
 
 echo Master IP is set to $(cat /tmp/minion/master_ip)
-echo Minion ID is archliveminion$(hostname -i)
+echo Minion ID is $(hostname)
 
 echo Starting the salt-minion service...
 systemctl start salt-minion
