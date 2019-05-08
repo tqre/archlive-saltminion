c1b4b74add0dd6d000f2927ffd5bbf86f1453c6d
tqre <tkuure@gmail.com>
2019-05-08 18:51:22 +0300

    modified minion init script

--- minion_init.sh
+++ minion_init.sh
@@ -4,6 +4,7 @@ echo Minion initialization script starting...
 # Set minion hostname to last digits of it's IP address
 hostnamectl --static set-hostname min-$(hostname -i | sed 's/.*[.]//')
 
+# Make salt configuration file
 cat > /etc/salt/minion << EOF
 master: $(cat /tmp/minion/master_ip)
 id: $(hostname)
@@ -14,8 +15,4 @@ sls_list:
 
 EOF
 
-echo Master IP is set to $(cat /tmp/minion/master_ip)
-echo Minion ID is $(hostname)
-
-echo Starting the salt-minion service...
 systemctl start salt-minion
